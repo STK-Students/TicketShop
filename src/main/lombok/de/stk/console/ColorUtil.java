a8e48cbffcbf7c0617c342395753d9eb857a6a40
SaltyAimbOtter <saltyaimbotter@gmail.com>
2021-01-21 12:37:26 +0100

    add delombok to enable javadocs build

--- src/main/lombok/de/stk/console/ColorUtil.java
+++ src/main/lombok/de/stk/console/ColorUtil.java
@@ -33,7 +33,7 @@ public class ColorUtil {
 
         //Normal Colours
         RED("\033[0;31m"),
-        GREEN("\033[0;32m"),
+        GREEN("\\u001B[32m"),
         YELLOW("\033[0;33m"),
         BLUE("\033[0;34m"),
         PURPLE("\033[0;35m"),
@@ -46,7 +46,7 @@ public class ColorUtil {
         BOLD_YELLOW("\033[1;33m"),
         BOLD_BLUE("\033[1;34m"),
         BOLD_PURPLE("\033[1;35m"),
-        BOLD_CYAN("\033[1;36m"),
+        BOLD_CYAN("\u001B[32m"),
         BOLD_WHITE("\033[1;37m"),
 
         //Underlined Colours
