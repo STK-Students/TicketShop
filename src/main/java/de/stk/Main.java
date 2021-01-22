60a28d90dc675290714be40cb9c56164df8447b9
SaltyAimbOtter <saltyaimbotter@gmail.com>
2021-01-22 09:22:13 +0100

    add error gui

--- src/main/java/de/stk/Main.java
+++ src/main/java/de/stk/Main.java
@@ -1,6 +1,9 @@
 package de.stk;
 
+import javax.swing.*;
 import java.io.IOException;
+
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
 
 /**
  * A wrapper class that open a new terminal session with the actual TicketShop.
@@ -9,6 +12,7 @@ public class Main {
 
     /**
      * Enables color code processing using the Windows registry and then starts the actual programm in a new command line.
+     *
      * @param args any arguments that might be passed using the command line are ignored.
      */
     public static void main(String[] args) {
@@ -17,9 +21,8 @@ public class Main {
             runtime.exec("reg add HKEY_CURRENT_USER\\Console /v VirtualTerminalLevel /t REG_DWORD /d 0x00000001 /f");
             runtime.exec("cmd.exe /c start java -cp STK-TicketShop.jar de.stk.TicketShop");
         } catch (IOException e) {
-            System.out.println("Die Windows Command Line konnte nicht gestartet werden." +
-                    " Bitte starten Sie das Programm als Administrator.");
-            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Die Windows Command Line konnte nicht gestartet werden.\n" +
+                    "Bitte starten Sie das Programm als Administrator.", "Fehler!", ERROR_MESSAGE);
         }
     }
 }
